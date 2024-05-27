c013f9f88f00d7e0e94ea9839d496d4f48c79b74
Alex_Samoylow <as960185@gmail.com>
2024-05-27 20:59:59 +0300

    Ver final.1

---
+++ src/FuncOrMeth.java
@@ -0,0 +1,29 @@
+public class FuncOrMeth {
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        System.out.println(time);
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = getSum(array);
+        System.out.println(sum);
+        Integer a = 0;
+        System.out.println(a);
+        fu(a);
+        System.out.println(a);
+        time = System.currentTimeMillis();
+        System.out.println(time);
+    }
+
+    public static int getSum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    private static void fu(Integer x) {
+        x++;
+        System.out.println(x);
+
+    }
+}
